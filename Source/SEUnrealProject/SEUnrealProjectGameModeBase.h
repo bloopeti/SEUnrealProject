73f851865574535d4e2d67d31fa280727234e523
bloopeti <peter.zavaczki@gmail.com>
2017-11-15 12:43:08 +0200

    Converting to C++ project

---
+++ Source/SEUnrealProject/SEUnrealProjectGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SEUnrealProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SEUNREALPROJECT_API ASEUnrealProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
